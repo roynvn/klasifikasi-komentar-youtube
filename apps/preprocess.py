83db3646f2b3ca23bb127b2a32d2ac1b049b3b94
Roy Noviantho <72556811+roynvn@users.noreply.github.com>
2021-06-07 19:44:13 +0700

    Update preprocess.py

--- apps/preprocess.py
+++ apps/preprocess.py
@@ -39,7 +39,8 @@ def app():
         my_bar = st.progress(0)
         
         if button_preprocessing:
-            st.info("Tahapan:\nCase Fold-->Tokenize-->Normalisasi-->Stopwords-->Stemming-->Clean-->Labelling")
+            st.info('''Tahapan: \n 
+                    Case Fold-->Tokenize-->Normalisasi-->Stopwords-->Stemming-->Clean-->Labelling'''")
             def case_fold(text):
                 #Menghapus HTTP yang tidak selesai
                 text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
